396d3ccb25e9f5fa5bb9171809583f7aad44c1bc
ThaiTran31 <thaitrancs31@gmail.com>
2023-03-09 00:17:03 +0700

    [ADD][BKF-1.2][BKF-1.3] Create models: product, category & product images handling & setting up admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
 
-# Register your models here.
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "product")
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "price")
+    prepopulated_fields = {"slug": ("name",)}
+    inlines = [ProductImageInline]
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 0
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title",)
+    inlines = [ProductInline]
+    prepopulated_fields = {"slug": ("title",)}
--- products/models.py
+++ products/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from django_jsonform.models.fields import ArrayField
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, null=True)
+    category = models.ForeignKey(Category, related_name='products', null=True, blank=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=9, decimal_places=2, default=99.99)
+    description = models.TextField(null=True, blank=True)
+    origin = models.CharField(max_length=255, null=True, blank=True)
+    colors = ArrayField(models.CharField(max_length=20), 6)
+    sizes = ArrayField(models.TextField(), 4)
+    is_custom_design = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return f'{self.product.name} - {self.id}'
