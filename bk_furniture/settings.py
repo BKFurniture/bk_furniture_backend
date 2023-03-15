fa9d6ffc4958d0a8c473a6690dced690c0193f1c
hoangduy181 <hoang.duy.181001@gmail.com>
2023-03-15 13:05:21 +0700

    [BKF] Update railway db

--- bk_furniture/settings.py
+++ bk_furniture/settings.py
@@ -85,14 +85,25 @@ WSGI_APPLICATION = "bk_furniture.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "BK_Furniture",
+#         "USER": "postgres",
+#         "PASSWORD": "123456",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "BK_Furniture",
-        "USER": "postgres",
-        "PASSWORD": "123456",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'nSKE0qCsjV0gmfABZd4n',
+        'HOST': 'containers-us-west-203.railway.app',
+        'PORT': '6206',
     }
 }
 
