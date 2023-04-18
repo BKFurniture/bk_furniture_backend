a46995705869d51014a80179693e60d0835622ea
hoangduy181 <hoang.duy.181001@gmail.com>
2023-04-18 09:40:14 +0700

    [FIX][BKF-103] Update chatbot
    Create prompt for more precise answer.
    Implement the webapp config for chatbot.

--- chatbot/BKF_chatbot.py
+++ chatbot/BKF_chatbot.py
@@ -4,13 +4,15 @@ import pandas as pd
 import json
 import tiktoken
 from django.conf import settings
+from django.apps import AppConfig
+
 
 COMPLETIONS_MODEL = "text-davinci-003"
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
 openai.api_key = settings.OPENAI_API_KEY
 
-MAX_SECTION_LEN = 200
+MAX_SECTION_LEN = 300
 SEPARATOR = "\n* "
 ENCODING = "gpt2"  # encoding for text-davinci-003
 
@@ -18,7 +20,6 @@ encoding = tiktoken.get_encoding(ENCODING)
 separator_len = len(encoding.encode(SEPARATOR))
 
 COMPLETIONS_API_PARAMS = {
-    # We use temperature of 0.0 because it gives the most predictable, factual answer.
     "temperature": 0.0,
     "max_tokens": 500,
     "model": COMPLETIONS_MODEL,
@@ -34,26 +35,16 @@ def get_embedding(text: str, model: str = EMBEDDING_MODEL) -> list[float]:
 
 
 def vector_similarity(x: list[float], y: list[float]) -> float:
-    """
-    Returns the similarity between two vectors.
-
-    Because OpenAI Embeddings are normalized to length 1, the cosine similarity is the same as the dot product.
-    """
     return np.dot(np.array(x), np.array(y))
 
 
 def order_document_sections_by_query_similarity(query: str, contexts: dict[(str, str), np.array]) -> list[(float, (str, str))]:
-    """
-    Find the query embedding for the supplied query, and compare it against all of the pre-calculated document embeddings
-    to find the most relevant sections.
-
-    Return the list of document sections, sorted by relevance in descending order.
-    """
     query_embedding = get_embedding(query)
 
-    document_similarities = sorted([
-        (vector_similarity(query_embedding, doc_embedding), doc_index) for doc_index, doc_embedding in contexts.items()
-    ], reverse=True)
+    document_similarities = sorted(
+        [
+            (vector_similarity(query_embedding, doc_embedding), doc_index) for doc_index, doc_embedding in contexts.items()
+        ], reverse=True)
 
     return document_similarities
 
@@ -67,11 +58,9 @@ def construct_prompt(question: str, context_embeddings: dict, df: pd.DataFrame) 
     chosen_sections = []
     chosen_sections_len = 0
     chosen_sections_indexes = []
+    chosen_sections_points = []
 
     for _, section_index in most_relevant_document_sections:
-        # Add contexts until we run out of space.
-        # print(section_index)
-        # print(_)
         section_index = int(section_index)
         document_section = df.loc[section_index]
 
@@ -81,15 +70,24 @@ def construct_prompt(question: str, context_embeddings: dict, df: pd.DataFrame) 
 
         chosen_sections.append(SEPARATOR + document_section.Content.replace("\n", " "))
         chosen_sections_indexes.append(str(section_index))
+        chosen_sections_points.append(_)
 
+    # if len(chosen_sections) == 0:
+    #     chosen_sections.append('No context given')
     # Useful diagnostic information
     print(f"Selected {len(chosen_sections)} document sections:")
     print("\n".join([
         f'{df.loc[int(idx)].Request}, {df.loc[int(idx)]["Type of request"]}, {df.loc[int(idx)]["Product"]}'
         for idx in chosen_sections_indexes
         ]))
-
-    header = """Answer the question as truthfully as possible using the provided context, use a gently voice as you are an customer caring employee, and if the answer is not contained within the text below, say "I don't know."\n\nContext:\n"""
+    print(chosen_sections_points)
+    header = """
+    We are BK Furniture store, we sells the best quality furnitures. We are here to help customer with your furniture needs.
+    Answer the question as truthfully as possible using the provided context, use a gently voice as you are an customer caring employee.
+    If the answer is not contained within the text below, say "I don't know".
+    If the question is not mentioning about our furniture or information about our store, say "I don't know".
+    If the question is not clear, say "I don't understand".
+    \n\nContext:\n"""
 
     return header + "".join(chosen_sections) + "\n\n Q: " + question + "\n A:"
 
@@ -120,7 +118,7 @@ def answer_query_with_context(
 class ChatBot:
 
     def __init__(self):
-
+        print('ok')
         self.df = pd.read_csv("chatbot\\data_folder\\BKF_Questions.csv")
         self.df['tokens'] = self.df.apply(
             lambda row: len(row.Content.split()),
@@ -134,7 +132,8 @@ class ChatBot:
             self.df,
             self.document_embeddings
         )
-# df = pd.read_csv('D:\\CODE\\.py\\Notebooks\\BKF_Questions.csv')
-# df['tokens'] = df.apply(lambda row: len(row.Content.split()) ,axis=1)
-# document_embeddings = json.load(open("doc_emb.json"))
-# print(answer_query_with_context("How is the delivery service of your store", df, document_embeddings))
+
+
+class WebappConfig(AppConfig):
+    name = 'davinci3'
+    chatbot = ChatBot()
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,15 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
 
-from .BKF_chatbot import ChatBot
-# Create your views here.
+
+from .BKF_chatbot import WebappConfig
 
 
 class ChatbotView(APIView):
-    chatbot = ChatBot()
 
     def post(self, request):
         message = request.data.get('message')
-        response = self.chatbot.answer_question(message)
+        response = WebappConfig.chatbot.answer_question(message)
         return Response({'response': response})
